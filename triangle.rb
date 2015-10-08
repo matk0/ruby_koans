23496036dca1ee5c345edaf85c6708f13bc51617
matk0 <matej@occupationbliss.com>
2015-10-08 07:18:55 +0200

    Passed about_triangle_project_2.rb

--- triangle.rb
+++ triangle.rb
@@ -14,6 +14,11 @@
 #   about_triangle_project_2.rb
 #
 def triangle(a, b, c)
+  
+  a, b, c = [a, b, c].sort
+
+  raise TriangleError if a <= 0 || a + b <= c
+
   if a == b && b == c
     return :equilateral
   elsif a == b || a == c || b == c
